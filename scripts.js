ba36aeb0a71ac4ee16e83feeedcab337cf83a1f1
Juan Carlos Venegas Aburto <89881553+venegas55@users.noreply.github.com>
2023-06-06 11:34:24 -0600

    solo una prueba

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+let name = prompt("como te llamas?")
+
+function verifyName (name) {
+    if (name.toLowerCase() === "annia") {
+        alert("perdoname por todo cielo, te amo mucho. Eres el amor de mi vida.")
+    } else {
+        alert("Esta página no es para ti")
+    }
+    
+}
+
+verifyName(name);
